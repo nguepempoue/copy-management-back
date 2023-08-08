97a3b0cad2f5a38374e0738b11b16a800f94d17c
nguepempoue <nguepempouejulio@gmail.com>
2023-08-08 16:22:23 +0200

    version après création du projet

---
+++ src/main/java/com/school/copyManagement/CopyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.school.copyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/copyManagement/CopyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.copyManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
